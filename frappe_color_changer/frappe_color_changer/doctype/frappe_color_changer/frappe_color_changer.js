0b2eb63b7860f76321da7afe378741b34b67667b
Alon Ben Refael <alonbr@proton.me>
2025-07-31 14:29:23 +0300

    Added DocType

---
+++ frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Alon Ben Refael and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Frappe Color Changer", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/test_frappe_color_changer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Alon Ben Refael and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFrappeColorChanger(FrappeTestCase):
+	pass
