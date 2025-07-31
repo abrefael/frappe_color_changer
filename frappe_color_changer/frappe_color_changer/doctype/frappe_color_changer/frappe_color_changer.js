9c836ecaa060f98609ed1dc836e1cdf7e1d25da3
Alon Ben Refael <alonbr@proton.me>
2025-07-31 20:34:24 +0300

    Update frappe_color_changer.js

--- frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.js
+++ frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2025, Alon Ben Refael and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Frappe Color Changer", {
-// 	refresh(frm) {
+frappe.ui.form.on("Frappe Color Changer", {
+	color_i(frm) {
+		frm.toggle_display("select_ii", true);
+		frm.toggle_display("color_ii", true);
+	},
+});
 
-// 	},
-// });
