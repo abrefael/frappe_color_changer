47a02d7ae7bc2c3d3ab51af702bb45dc0c054137
Alon Ben Refael <alonbr@proton.me>
2025-08-01 14:05:39 +0300

    Update frappe_color_changer.py

--- frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.py
+++ frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2025, Alon Ben Refael and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class FrappeColorChanger(Document):
 	pass
+	
+@frappe.whitelist()
+def update_template(doctype_name,element,color):
+	import os
+	file_path = '/files/css'
+	try:
+		os.mkdir(path)
+	except OSError as error:
+		pass
+	user_name = frappe.utils.get_fullname()
+	user_name = user_name.lower().replace(' ','_')
+	file_path += '/' + user_name + '_' + doctype_name + '.css'
