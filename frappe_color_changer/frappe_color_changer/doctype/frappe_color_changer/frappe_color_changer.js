77c84d6f507e3f9941b5bd8e43fed26b1833265f
Alon Ben Refael <alonbr@proton.me>
2025-08-03 10:11:08 +0300

    Update frappe_color_changer.js

--- frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.js
+++ frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2025, Alon Ben Refael and contributors
 // For license information, please see license.txt
-const roman=['i','ii','iii','iv','v','vi','vii','viii','ix','x'];
+
 var content;
 var url;
 var selections_options_lst = '\n.std-form-layout > .form-layout > .form-page\n.page-head\n.page-container\n.navbar\n.new-timeline .activity-title, .new-timeline .timeline-actions\nbody\na\n.btn.btn-default, div#driver-popover-item .driver-popover-footer button.btn-default\n.grid-footer .btn, .grid-footer div#driver-popover-item .driver-popover-footer button, div#driver-popover-item .driver-popover-footer .grid-footer button, .grid-custom-buttons .btn, .grid-custom-buttons div#driver-popover-item .driver-popover-footer button, div#driver-popover-item .driver-popover-footer .grid-custom-buttons button\n.grid-footer, .grid-custom-buttons\n.comment-box .comment-input-container .frappe-control .ql-editor\n.grid-heading-row\n.grid-body .data-row\n.grid-body\n.awesomplete .input-with-feedback\n.ql-toolbar.ql-snow\n.form-control\n.like-disabled-input\n.control-label\ninput[type="checkbox"]\n.frappe-control .ql-editor:not(.read-mode)\n.grid-body .data-row a';
@@ -8,13 +8,22 @@ var selections_options_lst = '\n.std-form-layout > .form-layout > .form-page\n.p
 frappe.ui.form.on("Frappe Color Changer", {
 	apply_btn(frm) {
 		var selected = frm.doc.select_i;
-		var child = frm.add_child(selections_chld_tbl);
+		let child = frm.add_child(selections_chld_tbl);
 		child.element = selected;
 		child.color = frm.doc.color_i;
-		product = '';
 		selections_options_lst = selections_options_lst.replace("\n" + selected + "\n", "\n");
-		frm.set_df_property(selection, "options", selections_options_lst);
-		frm.refresh_field(selection);
+		frm.set_df_property("select_i", "options", selections_options_lst);
+		frm.refresh_field("select_i");
+		frappe.call({method:'frappe_color_changer.frappe_color_changer.doctype.frappe_color_changer.frappe_color_changer.apply_color',
+			args: {
+				'element': selected,
+				'color':color,
+				'doctype_name': frm.doc.doctype_name,
+				'user_name': frm.doc.user_full_name
+			}
+		}).then(r => {
+			build_preview(frm);
+		});
 	},
 });
 
@@ -23,130 +32,77 @@ frappe.ui.form.on("selections_chld_tbl",{
 		var d = locals[cdt][cdn];
 		let element = d.element;
 		let color = d.color;
-		frm.get_field("selections_chld_tbl").grid.grid_rows[d.idx-1].remove();
+		frm.get_field("selections_chld_tbl").grid.grid_rows[d.idx - 1].remove();
+		selections_options_lst = selections_options_lst + "\n" + element;
+		frm.set_df_property("select_i", "options", selections_options_lst);
+		frm.refresh_field(selection);
 		frm.refresh();
-//		frappe.call({method:'frappe_color_changer.frappe_color_changer.doctype.frappe_color_changer.frappe_color_changer.Remove_Color',
-//			args: {
-//				'element': element,
-//				'color':color,
-//				'doctype_name': frm.doc.doctype_name
-//			}
-//		}).then(r => {
-//			build_preview(frm);
-//		});
+		frappe.call({method:'frappe_color_changer.frappe_color_changer.doctype.frappe_color_changer.frappe_color_changer.remove_color',
+			args: {
+				'element': element,
+				'color':color,
+				'doctype_name': frm.doc.doctype_name,
+				'user_name': frm.doc.user_full_name
+			}
+		}).then(r => {
+			build_preview(frm);
+		});
 	}
 })
 
-frappe.ui.form.on("Frappe Color Changer", {
-	color_i(frm) {
-		t_dsp('i',frm, frm.doc.select_i);
-	},
-});
 
-frappe.ui.form.on("Frappe Color Changer", {
-	color_ii(frm) {
-		t_dsp('ii',frm, frm.doc.select_ii);
-	},
-});
-
-frappe.ui.form.on("Frappe Color Changer", {
-	color_iii(frm) {
-		t_dsp('iii',frm, frm.doc.select_iii);
-	},
-});
-frappe.ui.form.on("Frappe Color Changer", {
-	color_iv(frm) {
-		t_dsp('iv',frm, frm.doc.select_iv);
-	},
-});
-frappe.ui.form.on("Frappe Color Changer", {
-	color_v(frm) {
-		t_dsp('v',frm, frm.doc.select_v);
-	},
-});
-frappe.ui.form.on("Frappe Color Changer", {
-	color_vi(frm) {
-		t_dsp('vi',frm, frm.doc.select_vi);
-	},
-});
-frappe.ui.form.on("Frappe Color Changer", {
-	color_vii(frm) {
-		t_dsp('vii',frm, frm.doc.select_vii);
-	},
-});
-frappe.ui.form.on("Frappe Color Changer", {
-	color_viii(frm) {
-		t_dsp('viii',frm, frm.doc.select_viii);
-	},
-});
-frappe.ui.form.on("Frappe Color Changer", {
-	color_ix(frm) {
-		t_dsp('ix',frm, frm.doc.select_ix);
-	},
-});
-
-
-function t_dsp(num, frm, selected){
-	let i = roman[roman.indexOf(num) + 1];
-	let selection = "select_" + i;
-	
-	frm.set_df_property(selection, "options", selections_options_lst);
-	frm.refresh_field(selection);
-	frm.toggle_display(selection, true);
-	frm.toggle_display("color_" + i, true);
-
-}
-
-function selections_options(frm){
-	var i = 0;
-	for (i; i<10; i++){
-		frappe.db.get_value('Frappe Color Changer', frm.doc.name , 'select_' + roman[i])
-		.then(r => {
-			selections_options_lst = selections_options_lst.filter(
-				(val) => val !== r.message['select_' + roman[i]]);
-		});
-	}
-	let selection = "select_" + roman[i-1];
-	frm.set_df_property(selection, "options", selections_options_lst);
-	frm.refresh_field(selection);
-
-}
 
 frappe.ui.form.on("Frappe Color Changer", {
 	doctype_name(frm) {
+		if (frm.doc.dt_selected){
+			frappe.throw(__('DocType was already set. Do not change it!'))
+		}
 		build_preview(frm);
 	},
 });
 
-function makeid(length) {
-	var result = '';
-	var characters = 'abcdefghijklmnopqrstuvwxyz';
-	var charactersLength = characters.length;
-	for ( var i = 0; i < length; i++ ) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
-}
 
 function build_preview(frm){
+	var id = '';
+	const characters = 'abcdefghijklmnopqrstuvwxyz';
+	const charactersLength = characters.length;
+	for ( let i = 0; i < 10; i++ ) {
+		id += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
 	url = '/app/' +
 		frm.doc.doctype_name.toLowerCase().replace(' ','-') +
 		'/new-' + frm.doc.doctype_name.toLowerCase().replace(' ','-') +
 		'-' +
-		makeid(10);
+		id;
 	content = ' <iframe style="width: 99%;" src="' + url + '"></iframe>';
 	frm.set_df_property("preview", "options", content);
 	frm.refresh_field("preview");
 }
 
 
+frappe.ui.form.on('Frappe Color Changer',
+	'before_save', function(frm){
+		if (frm.is_new()) {
+			frm.set_value('dt_selected',1);
+		}
+});
+
 
 frappe.ui.form.on("Frappe Color Changer", {
 	onload(frm) {
 		console.log(frappe.session.user_fullname);
-		if (!frm.is_new()){
+		if (frm.is_new()){
+			frm.set_value('user_full_name',frappe.user_info().fullname.toLowerCase().replace(' ','_');
+			frm.refresh_field('user_full_name');
+		}
+		else {
 			build_preview(frm);
-			selections_options(frm);
+			var tbl = frm.doc.selections_chld_tbl;
+			$.each(tbl, function(index, row){
+				selections_options_lst = selections_options_lst.replace("\n" + row.element + "\n", "\n");
+				frm.set_df_property("select_i", "options", selections_options_lst);
+				frm.refresh_field("select_i");
+			});
 		}
 	},
 });
