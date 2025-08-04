54bd193c7f68d2b17e4fd63b6415554089cb3e88
Alon Ben Refael <alonbr@proton.me>
2025-08-04 09:54:16 +0300

    Update frappe_color_changer.js

--- frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.js
+++ frappe_color_changer/frappe_color_changer/doctype/frappe_color_changer/frappe_color_changer.js
@@ -3,40 +3,7 @@
 
 var content;
 var url;
-var selections_options_lst = '\n.std-form-layout > .form-layout > .form-page:background-color\n.page-head:background-color\n.page-container\n.navbar\n.new-timeline .activity-title, .new-timeline .timeline-actions\nbody\na\n.btn.btn-default, div#driver-popover-item .driver-popover-footer button.btn-default\n.grid-footer .btn, .grid-footer div#driver-popover-item .driver-popover-footer button, div#driver-popover-item .driver-popover-footer .grid-footer button, .grid-custom-buttons .btn, .grid-custom-buttons div#driver-popover-item .driver-popover-footer button, div#driver-popover-item .driver-popover-footer .grid-custom-buttons button\n.grid-footer, .grid-custom-buttons\n.comment-box .comment-input-container .frappe-control .ql-editor\n.grid-heading-row\n.grid-body .data-row\n.grid-body\n.awesomplete .input-with-feedback\n.ql-toolbar.ql-snow\n.form-control\n.like-disabled-input\n.control-label\ninput[type="checkbox"]\n.frappe-control .ql-editor:not(.read-mode)\n.grid-body .data-row a';
-
-
-
-//	changeRule('.std-form-layout > .form-layout > .form-page', 'background-color', '#3584e4');// ✓ 
-//	changeRule('.page-head', 'background-color', '#3584e4');
-//	changeRule('.page-container', 'background-color', '#3584e4');
-//	changeRule('.navbar','background','#3584e4', 2);
-//	changeRule('.new-timeline .activity-title, .new-timeline .timeline-actions', 'background-color', '#3584e4');
-//	//changeRule('body','color','#ff7800');
-//	//changeRule('a','color','#ff7800');
-//	changeRule('.btn.btn-default, div#driver-popover-item .driver-popover-footer button.btn-default', 'background-color','#ff0000');
-//	changeRule('.grid-footer .btn, .grid-footer div#driver-popover-item .driver-popover-footer button, div#driver-popover-item .driver-popover-footer .grid-footer button, .grid-custom-buttons .btn, .grid-custom-buttons div#driver-popover-item .driver-popover-footer button, div#driver-popover-item .driver-popover-footer .grid-custom-buttons button', 'background-color','#ff0000');
-//	changeRule('.grid-footer, .grid-custom-buttons', 'background-color', '#3584e4');// ✓
-//	changeRule('.comment-box .comment-input-container .frappe-control .ql-editor', 'background-color', '#ff7800');
-//	changeRule('.comment-box .comment-input-container .frappe-control .ql-editor', 'color', '#613583');
-//	changeRule('.grid-heading-row', 'background-color', '#613583'); // ✓ table head
-//	changeRule('.grid-heading-row', 'color', '#f9f06b'); // ✓ table head
-//	changeRule('.grid-body .data-row','color','#f9f06b'); // ✓ table
-//	changeRule('.grid-body', 'background-color', '#613583');// ✓ table
-//	changeRule('.awesomplete .input-with-feedback', 'background-color', '#ff7800'); // ✓
-//	changeRule('.ql-toolbar.ql-snow', 'background-color', '#ff7800',2);
-//	changeRule('.awesomplete .input-with-feedback', 'color', '#613583');// ✓
-//	changeRule('.form-control', 'background-color', '#ff7800');// ✓
-//	changeRule('.form-control', 'color', '#613583');// ✓
-//	changeRule('.like-disabled-input', 'background-color', '#ff7800');// ✓
-//	changeRule('.like-disabled-input', 'color', '#613583');// ✓
-//	changeRule('.control-label', 'color', '#865e3c');// ✓
-//	changeRule('input[type="checkbox"]', 'boarder', '#e01b24');
-//	changeRule('.frappe-control .ql-editor:not(.read-mode)', 'background-color', '#ff7800');
-//	changeRule('.frappe-control .ql-editor:not(.read-mode)', 'color', '#613583');
-//	var styleSheet = document.createElement("style")
-//	styleSheet.textContent = '.grid-body .data-row a {color: #f9f06b;}'// ✓
-//	document.head.appendChild(styleSheet)
+var selections_options_lst = '\nPage background color\nPage background text color\nButtons background\nField background color\nField font color\nTable and Text Editor head background color\nTable rows font color\nTable head font color\nTable rows background color';
 
 
 
